2b15534732b15ebc9eb3cad9243ab6a19a7e74a5
mlegall@ippon.fr <mlegall@ippon.fr>
2023-11-21 09:05:34 +0000

    scenario 7

--- models/staging/base/base_orders.sql
+++ models/staging/base/base_orders.sql
@@ -1,14 +1,18 @@
-with base_orders as (
-select
-    identifier as id
-    , restaurant_identifier as restau_id
-    , payment_method as payment_method
-    , amount as amount
-    , created_at as created_at
-from
-    {{ source('SOURCE', 'orders') }}
-qualify 
-        row_number() over (partition by id order by id desc) = 1
+with source as (
+    {{ mockable_source('sample_orders','SOURCE','orders') }}
+)
+, base_orders as (
+    select
+        identifier as id
+        , restaurant_identifier as restau_id
+        , dishes_ids as dishes_ids
+        , payment_method as payment_method
+        , amount as amount
+        , created_at as created_at
+    from
+        source
+    qualify 
+            row_number() over (partition by id order by id desc) = 1
 )
 
 select * from base_orders
